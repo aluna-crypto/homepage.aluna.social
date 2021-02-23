643f291b1df790cee090915644c365d821c51498
Filipe Dias <mail@filipedias.me>
2021-02-23 19:32:25 +0000

    add metaTags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,48 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
+          <title>
+            Aluna Social : The Top Multi Exchange Crypto Trading Platform
+          </title>
+          <meta
+            name="description"
+            content="A crypto trading platform enabling traders to manage multiple exchange accounts from one place while leveraging social features and copy trading."
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            name="og:title"
+            property="og:title"
+            content="Aluna Social : The Top Multi Exchange Crypto Trading Platform "
+          />
+          <meta
+            name="og:description"
+            property="og:description"
+            content="A crypto trading platform enabling traders to manage multiple exchange accounts from one place while leveraging social features and copy trading."
+          />
+          <meta property="og:site_name" content="Aluna.social" />
+          <meta property="og:url" content="https://aluna.social" />
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:title"
+            content="Aluna Social : The Top Multi Exchange Crypto Trading Platform "
+          />
+          <meta
+            name="twitter:description"
+            content="A crypto trading platform enabling traders to manage multiple exchange accounts from one place while leveraging social features and copy trading."
+          />
+          <meta name="twitter:site" content="https://aluna.social" />
+          <meta name="twitter:creator" content="@AlunaSocial" />
+          <link rel="icon" type="image/png" href="favicon.ico" />
+          <link rel="apple-touch-icon" href="favicon.ico" />
+          <meta
+            property="og:image"
+            content="https://aluna.social/img/og-image-landscape.jpg"
+          />
+          <meta
+            name="twitter:image"
+            content="https://aluna.social/img/og-image-landscape.jpg"
+          />
+          <link rel="canonical" href="https://aluna.social" />
           <link
             rel="preload"
             href="/fonts/Roboto-Light.ttf"
