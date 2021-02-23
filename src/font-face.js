cd3d31d34b51c598cf6fc1bd827976ef2043f6cf
hems <hems.inlet@gmail.com>
2021-02-23 22:34:31 +0000

    updating font address

--- src/font-face.js
+++ src/font-face.js
@@ -3,20 +3,20 @@ export default `
       font-family: 'Roboto300';
       font-weight: 300;
       font-display: swap;
-      src: url(https://aluna-homepage.herokuapp.com/fonts/Roboto-Light.ttf);
+      src: url(https://aluna.social/fonts/Roboto-Light.ttf);
     }
     @font-face {
       font-family: 'Roboto';
       font-style: normal;
       font-weight: 400;
       font-display: swap;
-      src: url(https://aluna-homepage.herokuapp.com/fonts/Roboto-Regular.ttf);
+      src: url(https://aluna.social/fonts/Roboto-Regular.ttf);
     }
     @font-face {
       font-family: 'Roboto500';
       font-style: normal;
       font-weight: 500;
       font-display: swap;
-      src: url(https://aluna-homepage.herokuapp.com/fonts/Roboto-Medium.ttf);
+      src: url(https://aluna.social/fonts/Roboto-Medium.ttf);
     }
   `;
