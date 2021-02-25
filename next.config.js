202e6b23a680ad9293eaf492449161da88a16cec
hems <hems.inlet@gmail.com>
2021-02-25 15:10:35 +0000

    removing redirect

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/:slug([^.]{2,}$)",
-        destination: "https://app.aluna.social/:slug",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/:slug([^.]{2,}$)",
+  //       destination: "https://app.aluna.social/:slug",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
   assetPrefix: isProd ? "https://aluna-homepage.herokuapp.com/" : "",
 };
